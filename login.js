ad5e80a966bc6a4062f1cbc3067eb1423a4815d0
Guoshuai <msn4399@163.com>
2019-01-24 23:30:51 +0800

    press key to continue

--- login.js
+++ login.js
@@ -7,31 +7,41 @@
 
 const axios = require('axios');
 
-let d = "action=login&username="+"316010XXXX"+
-    "&password="+"XXXXXX"+
-    "&ac_id=3"+
-    "&user_ip="+
-    "&nas_ip="+
-    "&user_mac="+
-    "&save_me=0"+
+const waitForKeyPress = () => {
+    console.log('Press any key to exit');
+    process.stdin.setRawMode(true);
+    process.stdin.resume();
+    process.stdin.on('data', process.exit.bind(process, 0));
+};
+
+let d
+    = "action=login&username=" +
+    "316010XXXX" +
+    "&password=" +
+    "XXXXXX" +
+    "&ac_id=3" +
+    "&user_ip=" +
+    "&nas_ip=" +
+    "&user_mac=" +
+    "&save_me=0" +
     "&ajax=1";
 
 axios({
-    method: "post",
-    url: "https://net.zju.edu.cn/include/auth_action.php",
-    action: 'login',
-    data: d,
+    method : "post",
+    url : "https://net.zju.edu.cn/include/auth_action.php",
+    action : 'login',
+    data : d,
 }).then((res) => {
     let ok_pattern = /^login_ok,/;
     let interval_pattern = /^E2532/;
-    if(ok_pattern.test(res.data)) {
+    if (ok_pattern.test(res.data)) {
         console.log("Login OK!");
-    }
-    else if (interval_pattern.test(res.data)) {
-        console.log("两次认证间隔时间太短！");
-    }
-    else {
+        waitForKeyPress();
+    } else if (interval_pattern.test(res.data)) {
+        console.log("Interval too short! Wait for 10 seconds...");
+        waitForKeyPress();
+    } else {
         console.log(res.data);
+        waitForKeyPress();
     }
 });
-
